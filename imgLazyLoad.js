ea5b71c174736c06b83315c5259076014232d4f6
ChaosAlphard <Alphard450@outlook.com>
2019-07-21 23:03:26 +0800

    添加注释

--- imgLazyLoad.js
+++ imgLazyLoad.js
@@ -1,6 +1,5 @@
 /**
  * 使用IntersectionObserver 判断图片是否进入可视区域
- * 进入可视区域, 从dataset 中拿到对应的imgsrc 替换原有的src
  * Simple:
  * const lazyimg = Array.from(document.querySelectorAll('img[data-imgsrc]'))
  * const ob = getObServer()
@@ -11,10 +10,17 @@
 function getObServer() {
   return new IntersectionObserver( ioes => {
     ioes.forEach( ioe => {
+      // target:
+      // 当前正在观察的元素
       const elem = ioe.target
+      // intersectionRatio: (返回值介于0-1之间)
+      // 当前正在观察的元素有多少部分是可见的
       const interRatio = ioe.intersectionRatio
-      if(interRatio>0&&interRatio<1) {
+      // intersectionRatio 大于0 则表示元素可见
+      if(interRatio>0&&interRatio<=1) {
+        // 将img 元素中的src 替换为img 元素dataset 中的imgsrc
         elem.src = elem.dataset.imgsrc
+        // onload 或者onerror 时停止监视对象
         elem.onload = elem.onerror = () => ob.unobserve(elem)
       }
     })
